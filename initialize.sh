053c9cc5d937cf8ee1a41d5e7b56b294961984e2
dinsizimansiz <oguzvuruskaner/@gmail.com>
2018-10-26 03:14:37 +0300

    Added new accounts to network.

--- initialize.sh
+++ initialize.sh
@@ -15,9 +15,11 @@ cleos wallet import --private-key=5Hvs15GhqPKGH3iPjCz6nzRHYxXJqkDPxvVQWVB7wcKfHW
 cleos wallet import --private-key=5HtCgqJjujYuWA1ztAWa53SJV38ZFaQNtSXr4HLqB1X8ArrACr9 -n eosbattleship
 cleos wallet import --private-key=5J3duNdBznuT1rrF3te7wDwmQaoVHfvLNTUyxQ33MChFRNhAmeK -n eosbattleship
 cleos wallet import --private-key=5KjbLYRvvrd7aH2xyf6kKSVsecVRpP3cSAbvtwibGHFQDC1EzYr -n eosbattleship
+cleos wallet import --private-key=5KYuC5BftZN4rt5N4F6VYZoa9YERV1rLHLnx6ehGfN8thMWyrmy -n eosbattleship #Bir
+cleos wallet import --private-key=5KYZnRZiRc7JQDq1zrik4pYJaHLfZHP3PUSVhqRekXvjMyaGSkf -n eosbattleship #Iki
+cleos wallet import --private-key=5JMXkyceHKEprkg7SPPER8ZutyvHp1DfMwYc1JggChPDHhSVvDw -n eosbattleship #Uc
 
-
-cleos create account eosio game EOS5iWjRDzCmMqS2RvNviwUJR8Lc5DoA3HcesWpz63mXQ554mMoos EOS5iWjRDzCmMqS2RvNviwUJR8Lc5DoA3HcesWpz63mXQ554mMoos
+cleos create account eosio battleship EOS5iWjRDzCmMqS2RvNviwUJR8Lc5DoA3HcesWpz63mXQ554mMoos EOS5iWjRDzCmMqS2RvNviwUJR8Lc5DoA3HcesWpz63mXQ554mMoos
 
 cleos create account eosio nese EOS7Z5uXqVVmKHurZ8Rn5uWTqidfLF1TQmF8mcXYqXJyTrZgXVeBm EOS7Z5uXqVVmKHurZ8Rn5uWTqidfLF1TQmF8mcXYqXJyTrZgXVeBm
 
@@ -27,12 +29,20 @@ cleos create account eosio tester EOS5RAptLnksMFsZpUxeSeWmHFqrEiJTt18Pr2YyreWq87
 
 cleos create account eosio printer EOS5coN3CvRoXfNaeWZD6Cfp8AtcX1U5vgreBin561zaFCbPvfWvv EOS5coN3CvRoXfNaeWZD6Cfp8AtcX1U5vgreBin561zaFCbPvfWvv
 
+cleos create account eosio bir EOS4xzFtwsRrxhckv21f6aZQeSo8pgQdGT8dsPpy9RjDh4Zxn8Gmt EOS4xzFtwsRrxhckv21f6aZQeSo8pgQdGT8dsPpy9RjDh4Zxn8Gmt
+
+cleos create account eosio iki EOS5hH9TJ5vUSvgFcDkUHKcVwwf9hHzmcrjMjUjX8U3scsv2yVRCA EOS5hH9TJ5vUSvgFcDkUHKcVwwf9hHzmcrjMjUjX8U3scsv2yVRCA
+
+cleos create account eosio uc EOS8E4yVb4GA6H1GwQY3TUuFZXkoUVo9r33crCUxrkmH1Afb8vmjs EOS8E4yVb4GA6H1GwQY3TUuFZXkoUVo9r33crCUxrkmH1Afb8vmjs
+
+
+
 cd ~/Desktop/prog/proj/eos/eosbattleship/
 
 eosiocpp -o contract.wast contract.cpp
 eosiocpp -g contract.abi contract.cpp
 
-cleos set contract game . ./contract.wasm ./contract.abi
+cleos set contract battleship  . ./contract.wasm ./contract.abi
 
 
 
